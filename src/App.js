50636f4c51086c82cda51ec42bca2bac4ad24351
sazzad <sajusajjad052@gmail.com>
2022-09-29 23:08:09 +0600

    'question'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./component/Header/Header";
 import Product from "./component/product/Product";
+import Question from "./component/question/Question";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Product></Product>
+      <Question></Question>
     </div>
   );
 }
