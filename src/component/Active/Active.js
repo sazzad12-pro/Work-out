ba5fc7463c840c7434598e4e37782e8346ab9dd8
sazzad <sajusajjad052@gmail.com>
2022-09-29 22:59:28 +0600

    'toast'

--- src/component/Active/Active.js
+++ src/component/Active/Active.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./active.css";
 
 const Active = () => {
+  const showToast = () => toast("Wow so easy! interesting");
   return (
-    <div className="mt-5  ms-3 me-3 active-item">
-      <button>Active Completed</button>
-    </div>
+    <>
+      {" "}
+      <div className="mt-5  ms-3 me-3 active-item">
+        <button onClick={showToast}>Active Completed</button>
+      </div>
+      <ToastContainer></ToastContainer>
+    </>
   );
 };
 
